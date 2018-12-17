20ee776e37dd55d57083f3e3dc3be991d7a1bae0
Gregorio Manabat <gregoriomanabat@gmail.com>
2018-12-17 18:40:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-function theBe
+function theBeatlesPlay(musicians, instruments){
+  let arr = [];
+  for(let i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts){
+  let arr = [];
+  let counter = facts.length;
+  while(counter) > 0){
+    arr.push(`${facts[counter]}!!!`);
+    counter--;
+  }
+  return arr;
+}
